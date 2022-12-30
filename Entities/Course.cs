57f9255443d0c0c3e0328fd068effb5b3e61c17b
nurayklc <nuraykilic0101@gmail.com>
2022-12-30 13:00:02 +0300

    Add project files.

---
+++ Entities/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstWithEfCore.Entities
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeFirstWithEfCore.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrolmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
