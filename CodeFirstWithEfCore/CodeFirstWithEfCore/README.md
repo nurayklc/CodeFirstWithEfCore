1c32b4383727726e5c95d1c25fb129aebc10c5a5
nurayklc <nuraykilic0101@gmail.com>
2023-02-28 14:55:46 +0300

    Add serilog demo project

---
+++ CodeFirstWithEfCore/CodeFirstWithEfCore/README.md
@@ -0,0 +1,17 @@
+# Code First With EF Core
+ - An API is designed in this repository, where Code First method with Entity Framework are made.
+---
+## Used Techs
+- .Net 6 WebAPI
+- Entity Framework Core
+- Dependency Injection
+- Swagger
+
+## Run Project
+```
+add-migration commandName
+update-database
+```
+Successfully created database.
+
+#### Done.
---
+++ SerilogWithNet6/SerilogDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace SerilogDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            try
+            {
+                 _logger.LogInformation("Get method executing...");
+                Log.Information("Get method executing...");
+                Log.Warning("Warning message...");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            .ToArray();
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"{ex.Message}");
+                throw new Exception();
+            }
+        }
+    }
+}
+
